d8bbd9a5dd3cba43585535a73cf4987ef91676c8
Tiago <tiagopiresdasilva@gmail.com>
2016-02-22 09:53:36 +0000

    final

--- src/org/academia/tiagopires/game/Sound.java
+++ src/org/academia/tiagopires/game/Sound.java
@@ -1,6 +1,7 @@
 package org.academia.tiagopires.game;
 
 import java.io.*;
+import java.net.URL;
 
 import sun.audio.*;
 
@@ -19,8 +20,18 @@ public class Sound {
     public static void sound(String path)
             throws Exception {
         // open the sound file as a Java input stream
-        String gongFile = path;
-        InputStream in = new FileInputStream(gongFile);
+        InputStream in;
+        URL soundURL = Sound.class.getResource(path.startsWith("/")?path:"/" + path);
+
+      //  System.out.println("opening " + path);
+
+        if (soundURL == null) {
+            //System.out.println("opening from file");
+            in = new FileInputStream(path);
+        } else {
+            //System.out.println("opening from jar");
+            in = soundURL.openStream();
+        }
 
         // create an audiostream from the inputstream
         AudioStream audioStream = new AudioStream(in);
